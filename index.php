c9c1ebf398a11615151ed024afd392e1411d59be
Bayu Navanto Sadiq <snavanto@gmail.com>
2020-10-13 08:27:10 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Baru</title>
+</head>
+<body>
+    <h1>Git local to github</h1>
+</body>
+</html>
